349c5817383b4b2ec01c65f9fcf92af2556c2de7
Author fenixwu <fenixwu.work@gmail.com>
2018-11-21 11:03:55 +0800

    簡單 exchange 範例

---
+++ send/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+const exchangeName = "test"
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		exchangeName,        // name
+		amqp.ExchangeFanout, // type
+		false,               // durable
+		false,               // auto-deleted
+		false,               // internal
+		false,               // no-wait
+		nil,                 // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	type message struct {
+		Name string `json:"name"`
+		Num  int    `json:"num"`
+	}
+
+	for i := 0; i <= 10; i++ {
+		msg := message{"YO", i}
+		data, err := json.Marshal(&msg)
+		failOnError(err, "Marshal error")
+		publish(ch, exchangeName, []byte(data))
+	}
+}
+
+func publish(ch *amqp.Channel, exchangeName string, message []byte) {
+	err := ch.Publish(
+		exchangeName, // exchange
+		"",           // routing key
+		false,        // mandatory
+		false,        // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        message,
+		})
+	failOnError(err, "Failed to publish a message")
+}
