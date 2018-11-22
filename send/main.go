b9342eee4e826eb375f3b8670b5f9ee070523fb1
Author fenixwu <fenixwu.work@gmail.com>
2018-11-22 14:07:26 +0800

    延遲執行食重試機制雛形

--- send/main.go
+++ send/main.go
@@ -7,6 +7,12 @@ import (
 	"github.com/streadway/amqp"
 )
 
+const (
+	exchangeName      = "master"
+	exchangeFaileName = "master.failed"
+	exchangeRetryName = "master.retry"
+)
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
@@ -14,48 +20,66 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	// ----- Connect -----
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
+	// ----- Open Channel -----
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	err = ch.ExchangeDeclare(
-		"test",              // name
-		amqp.ExchangeFanout, // type
-		true,                // durable
-		false,               // auto-deleted
-		false,               // internal
-		false,               // no-wait
-		nil,                 // arguments
-	)
+	// ----- Declare Exchange -----
+	err = declareExange(ch, exchangeName, amqp.ExchangeTopic)
+	failOnError(err, "Failed to declare an exchange")
+
+	err = declareExange(ch, exchangeFaileName, amqp.ExchangeTopic)
+	failOnError(err, "Failed to declare an exchange")
+
+	err = declareExange(ch, exchangeRetryName, amqp.ExchangeTopic)
 	failOnError(err, "Failed to declare an exchange")
 
 	type message struct {
-		Name string `json:"name"`
-		Num  int    `json:"num"`
+		Name   string `json:"name"`
+		Num    int    `json:"num"`
+		Status string `json:"status"`
 	}
 
 	for i := 0; i <= 10; i++ {
-		msg := message{"YO", i}
+		s := "success"
+		if i == 5 {
+			s = "error"
+		}
+		msg := message{"WEEEEEEEEEE!", i, s}
 		data, err := json.Marshal(&msg)
 		failOnError(err, "Marshal error")
-		publish(ch, "test", []byte(data))
+		publish(ch, exchangeName, []byte(data))
 	}
 }
 
 func publish(ch *amqp.Channel, exchangeName string, message []byte) {
 	err := ch.Publish(
-		"test", // exchange
-		"",     // routing key
-		false,  // mandatory
-		false,  // immediate
+		exchangeName,       // exchange
+		"task.event.creat", // routing key
+		false,              // mandatory
+		false,              // immediate
 		amqp.Publishing{
+			DeliveryMode: 2, // persistence
 			ContentType:  "application/json",
-			DeliveryMode: 2,
 			Body:         message,
 		})
 	failOnError(err, "Failed to publish a message")
+}
+
+func declareExange(ch *amqp.Channel, name, exchangeType string) error {
+	return ch.ExchangeDeclare(
+		name,         // name
+		exchangeType, // type
+		true,         // durable
+		false,        // auto-deleted
+		false,        // internal
+		false,        // no-wait
+		nil,          // arguments
+	)
 }
